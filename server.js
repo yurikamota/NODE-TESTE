f2b22e1d39d132b9605ebafdce845b25cc5250f3
Mac <yurika_mota@outlook.com>
2026-04-22 21:01:37 -0300

    database-memory.js

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-import { createServer} from 'node:http';
+// import { createServer} from 'node:http';
 
-const server = createServer((request, response) => {
-    response.write('Hello World!');
-    return response.end();
-})
+// const server = createServer((request, response) => {
+//    response.write('Hello World!');
+// return response.end();
+// })
 
-server.listen(3333); // http://localhost:3333
+// server.listen(3333); // http://localhost:3333
+
+import { fastify } from 'fastify';
+import { DatabaseMemory } from './database-memory.js';
+
+const server = fastify();
+
+// GET, POST, PUT, PATCH, DELETE
+
+// Route Parameter
+
+server.get('/', () => {
+    return 'Hello World!';
+});
+
+server.get('/hello', () => {
+    return 'Hello Yurika';
+});
+
+server.get('/node', () => {
+    return 'Hello Node.js';
+});
+
+server.listen({
+    port: 3333,
+});
